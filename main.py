1ff386c008b7ecb224b0182af96ef6d21904ef6d
statelinecat <statelinecat@gmail.com>
2024-07-05 02:31:28 +0300

    add manage stores

--- main.py
+++ main.py
@@ -27,17 +27,60 @@ class Store:
         else:
             print(f"Товар '{item_name}' не найден в ассортименте.")
 
-# Пример использования
-store = Store("Магазин 'Свет'","ул. Светлая, д. 15")
+stores = []
 
-store.add_item("яблоки", 0.5)
-store.add_item("бананы", 0.75)
+while True:
+    doit2 = '3'
+    doit1 = input(f"Для добавления нового магазина введите 1 \n"
+                  f"Для управления существующим магазином введите 2 \n"
+                  f"Для вывода списка магазинов на экран введите 3 \n"
+                  f"Для выхода введите 0: ")
+    match doit1:
+        case '0':
+            break
+        case '1':
+            name = input('Введите название магазина:')
+            address = input('Введите адрес магазина:')
+            stores.append(Store(name, address))
+        case '2':
+            market_name = input('Введите название магазина, которым будем управлять:')
+            for market in stores:
+                if market_name == market.name:
+                    name = market
+                #else
+            while doit2 != '0':
+                doit2 = input(f"Для добавления нового товара в ассортимент магазина введите 1 \n"
+                      f"Для удаления товара из ассортимента магазина введите 2 \n"
+                      f"Для вывода полного списка товаров в магазине на экран введите 3 \n"
+                      f"Для изменения цены на товар введите 4 \n"
+                      f"Вывести цену конкретного товара на экран введите 5 \n" 
+                      f"Для выхода введите 0: ")
+                match doit2:
+                    case '0':
+                        print(f'Управление магазином {name.name} завершено')
+                    case '1':
+                        new_item = input('Введите название товара:')
+                        new_item_price  = input('Введите цену товара:')
+                        name.add_item(new_item, new_item_price)
+                        print(f'Товар {new_item} введен в ассортимент магазина {name.name} по цене {new_item_price} руб.')
+                    case '2':
+                        item_to_del = input('Введите название товара:')
+                        name.remove_item(item_to_del)
+                        print(f'Товар {item_to_del} выведен из ассортимента магазина {name.name}')
+                    case '3':
+                        for item_to_print in name.items:
+                            print(f'{item_to_print} - {name.get_price(item_to_print)} руб.')
+                    case '4':
+                        item_price_to_change = input('Введите название товара:')
+                        item_new_price = input('Введите новую цену товара:')
+                        name.update_price(item_price_to_change, item_new_price)
+                        print(f'Новая цена товара {item_price_to_change}: {name.get_price(item_price_to_change)} руб.')
+                    case '5':
+                        item_price_to_print = input('Введите название товара:')
+                        print(f'Цена товара {item_price_to_print}: {name.get_price(item_price_to_print)} руб.')
+        case '3':
+            for market in stores:
+                print(f'{market.name}, {market.address}')
+        case _:
+            print(stores)
 
-print(store.get_price("яблоки"))  # Выведет 0.5
-print(store.get_price("орехи"))  # Выведет None, так как такого товара нет
-
-store.update_price("бананы", 0.80)
-print(store.get_price("бананы"))  # Выведет 0.8
-
-store.remove_item("бананы")
-print(store.get_price("бананы"))  # Выведет None, так как товар был удален
